d6769b88a09706b55fbffc244bd26f72a18ca1d6
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-03-04 14:39:18 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Griffin Baker on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
